119ea982fed12866ce1c5a17f9144bba8eb585c8
CheezItMan <chris@adadevelopersacademy.org>
2017-10-30 15:45:34 -0700

    Finished refactor.

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,10 +1,23 @@
 class ChatController < ApplicationController
   def index
+    @channels = SlackApiWrapper.list_channels
   end
 
   def new
+    @channel = params["channel"]
   end
 
   def create
+    result = SlackApiWrapper.send_msg params["channel"], params["message"]
+
+    if result
+      flash[:success] = "Message Posted!"
+    else
+      flash[:error] = "Error!"
+    end
+
+    redirect_to root_path
+
+
   end
 end
